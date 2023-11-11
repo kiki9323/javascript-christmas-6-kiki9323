0997d0770565cd74f633efb3216432514d00c52b
noodle <noodle>
2023-11-12 06:25:33 +0900

    refactor: error문 추가

--- src/Lib/constants/error.js
+++ src/Lib/constants/error.js
@@ -5,6 +5,7 @@ const date = Object.freeze({
 
 const order = Object.freeze({
   invalid: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  invalidOnlyDrinks: '음료만 주문할 수 없습니다. 다시 입력해 주세요.',
 });
 
 const inputView = Object.freeze({
