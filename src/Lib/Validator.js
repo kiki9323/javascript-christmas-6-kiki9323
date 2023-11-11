04c43768272c7e83e4f5f9689f0fb9c89f30cc1c
noodle <noodle>
2023-11-12 06:03:16 +0900

    feat: validator에 섞여있던 로직을 정리하여 OrderManager로 분리

--- src/Lib/Validator.js
+++ src/Lib/Validator.js
@@ -1,69 +1,91 @@
 import AppError from '../errors/error.js';
 import Date from './constants/date.js';
 import ERROR from './constants/error.js';
-import MenuPrices from './constants/MenuPrize.js';
 
 const Validator = {
-  isValidDate(num) {
-    return Number(num) && num >= Date.period.start && num <= Date.period.end;
-  },
-
   validatedInputDate(value) {
     if (!this.isValidDate(value)) {
       throw new AppError(ERROR.inputView.date.invalidDate);
     }
+
+    return value;
+  },
+
+  isValidDate(num) {
+    return Number(num) && num >= Date.period.start && num <= Date.period.end;
+  },
+
+  /**
+   * TODO: 단일 책임 원칙에 따라 분리하기
+   *
+   * // Validator.js/validatedOrderMenuCount()
+   *
+   * 1. 문자열 파싱 parseOrderString
+   * 2. 주문 항목 검증
+   *    1. 주문 포맷 확인
+   *        > 안 되는 예시 '음식01,음식_3'
+   *    2. 입력형태 검증
+   *        > '음식1-1,음식2-2,음식3-3'
+   *        - 숫자가 0이상
+   *        - 공백 없어야 함
+   *        - 중복 메뉴 안 됨
+   *    -> 검증완료된 가공 데이터를 OrderManager.createOrderData() 에게 넘김.
+   *
+   *
+   * // OrderManager.js
+   * 3. 주문 데이터 생성 createOrderData -> OrderManager.js로 옮기기
+   *    orderManager에서 체크할 것들
+   *    1. 메뉴에 없는 음식인지
+   *    2. 음료수만 시키는 경우
+   *    3. 음료만 주문한 경우 검사 checkOnlyDrinks
+   */
+  validatedOrderMenuCount(orderString) {
+    const items = this.parseOrderString(orderString);
+    const checkDuplicateArray = [];
+
+    for (let i = 0; i < items.length; i++) {
+      this.validateOrderFormat(items[i]);
+      this.validateNonZeroQuantity(items[i]);
+      this.checkForDuplicates(items[i], checkDuplicateArray);
+    }
+
+    return items;
   },
 
   isOnlyNumber(num) {
     const numberRegex = /^\d+$/;
+
     return numberRegex.test(num);
   },
 
-  validatedOrderMenuAcount(orderString) {
-    const orders = [];
-    let hasOnlyDrinks = true;
+  parseOrderString(string) {
+    return string.split(',');
+  },
 
-    const items = orderString.split(','); // 입력값 파싱
+  validateOrderFormat(item) {
+    const regex = /^([a-zA-Z가-힣]+-\d+(,|$))+$/;
 
-    for (let item of items) {
-      const [menu, quantity] = item.split('-');
-
-      // quantity 예외처리
-      if (!this.isOnlyNumber(quantity)) throw new AppError(ERROR.inputView.order.invalid);
-
-      const category = Object.keys(MenuPrices).find((currentCategory) =>
-        Object.keys(MenuPrices[currentCategory]).includes(menu),
-      );
-
-      if (!category) {
-        throw new AppError(ERROR.inputView.order.invalid);
-      }
-
-      // 주문 객세 생성 및 계산
-      const onePrize = MenuPrices[category][menu];
-      const totalPrize = onePrize * parseInt(quantity);
-
-      const orderData = {
-        category,
-        name: menu,
-        quantity: parseInt(quantity),
-        onePrize,
-        totalPrize,
-      };
-
-      // 주문 정보를 배열에 추가
-      orders.push(orderData);
-
-      // 음료 외의 카테고리가 존재할 경우 플래그를 false로 설정
-      if (category !== '음료') hasOnlyDrinks = false;
+    if (!regex.test(item)) {
+      // throw new AppError('"음식메뉴-숫자" 와 같은 형식으로 공백 없이 입력해주세요.');
+      throw new AppError('유효하지 않은 주문입니다. 다시 입력해 주세요.');
     }
+  },
 
-    // 음료만 주문한 경우 에러 발생
-    if (hasOnlyDrinks && orders.length > 0) {
-      throw new AppError(`음료만 주문할 수 없습니다.`);
+  validateNonZeroQuantity(item) {
+    const [_, quantity] = item.split('-');
+
+    if (quantity === '0' || quantity === 0) {
+      throw new AppError('1개 이상 부터 주문 가능합니다.');
     }
+  },
 
-    return orders;
+  checkForDuplicates(item, checkDuplicateArray) {
+    const [menu, _] = item.split('-');
+
+    if (checkDuplicateArray.includes(menu)) {
+      throw new AppError('중복된 메뉴가 있습니다.');
+    }
+    checkDuplicateArray.push(menu);
   },
 };
 
--- src/Service/OrderManager.js
+++ src/Service/OrderManager.js
@@ -1,8 +1,58 @@
+import AppError from '../errors/error.js';
+import ERROR from '../Lib/constants/error.js';
+import MenuPrices from '../Lib/constants/MenuPrize.js';
+
 class OrderManager {
   #view;
 
   constructor(view) {
     this.#view = view;
+  }
+
+  createOrderData(items) {
+    // TODO: 입력받은(유효성 검증이 끝난 데이터 받아서 order data 만들기)
+    const orders = [];
+    let hasOnlyDrinks = true;
+
+    for (let item of items) {
+      const [menu, quantity] = item.split('-');
+      const category = this.validateCategoryAndMenu(menu);
+
+      // order data 생성
+      const onePrice = MenuPrices[category][menu];
+      const totalPrice = onePrice * parseInt(quantity);
+
+      const orderData = {
+        category,
+        name: menu,
+        quantity: parseInt(quantity),
+        onePrice,
+        totalPrice,
+      };
+
+      // 주문 정보를 배열에 추가
+      orders.push(orderData);
+
+      // 음료 외의 카테고리가 존재할 경우 플래그를 false로 설정
+      if (category !== '음료') hasOnlyDrinks = false;
+    }
+
+    // 음료만 주문한 경우 에러 발생
+    if (hasOnlyDrinks && orders.length > 0) {
+      throw new AppError(ERROR.inputView.order.invalidOnlyDrinks);
+    }
+
+    return orders;
+  }
+
+  validateCategoryAndMenu(menu) {
+    const categories = Object.keys(MenuPrices);
+    for (const category of categories) {
+      if (MenuPrices[category].hasOwnProperty(menu)) {
+        return category;
+      }
+    }
+    throw new AppError(ERROR.inputView.order.invalid);
   }
 
   validateTotalQuantity(orderedMenuAndCount) {
@@ -13,25 +63,25 @@ class OrderManager {
   }
 
   calculateTotalOrderAmount(orderedMenuAndCount) {
-    return orderedMenuAndCount.reduce((sum, order) => sum + order.totalPrize, 0);
+    return orderedMenuAndCount.reduce((sum, order) => sum + order.totalPrice, 0);
   }
 
   printOrderResult(orderedMenuAndCount, totalPrizeBeforeDiscount, isGiftEligible) {
     this.#view.printOrderHeader();
 
     // TODO: 문자열이 섞인 부분들 어떻게 리팩토링 할 수 있을지 고민하기.
-    // Order 사용자 주문
+    // Order <주문 메뉴>
     orderedMenuAndCount.forEach((order) => {
       this.#view.print(`${order.name} ${order.quantity}개`);
     });
 
-    // Order 사용자 주문
+    // Order <할인 전 총주문 금액>
     this.#view.printTotalAmountBeforeDiscount();
     this.#view.print(`${totalPrizeBeforeDiscount.toLocaleString()}원`);
 
-    // Order 후 총 금액에 따라서 샴페인 증정.
+    // Order 후 총 금액에 따라서 <증정 메뉴>
     this.#view.printGiftMenu();
-    this.#view.print(isGiftEligible ? `${'샴페인 1개'}` : '없음');
+    this.#view.print(isGiftEligible ? '샴페인 1개' : '없음');
   }
 }
 
