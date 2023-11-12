835ab267bd2ffb8334b6957d3f00c28fc1e860a3
noodle <noodle>
2023-11-13 00:41:59 +0900

    refactor: 사용하지 않는 utils 제거

--- src/Lib/utils.js
+++ src/Lib/utils.js
@@ -1,4 +1,3 @@
-// import Badges from './constants/Badge.js';
 import OutputView from '../View/OutputView.js';
 
 export const deepFreeze = (object) => {
@@ -20,25 +19,3 @@ export const inputWithRetry = async (inputMethod) => {
 };
 
 export const formatCommas = (prize) => prize.totalLocaleString();
-
-// // 혜택 내역 message
-// export const printDiscountMessage = (appliedDiscount, totalPrizeBeforeDiscount) => {
-//   let message = '';
-//   if (totalPrizeBeforeDiscount < 10000) return '없음';
-
-//   for (const [discountType, discountAmount] of Object.entries(appliedDiscount)) {
-//     if (discountAmount !== 0) {
-//       message += `${discountType}: -${discountAmount.toLocaleString()}원\n`;
-//     }
-//   }
-//   return message;
-// };
-
-// // 배지
-// export const getBadgeName = (totalBenefit) => {
-//   for (let i = Badges.length - 1; i >= 0; i--) {
-//     const badge = Badges[i];
-//     if (totalBenefit >= badge.threshold) return badge.name;
-//   }
-//   return Badges[0].name; // 기본값
-// };
