d9563b0af611a486a3ce34a910c6dc56f5999fe1
noodle <noodle>
2023-11-13 09:09:14 +0900

    refactor: App.js 불필요 코드 정리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Discount from './Domain/Discount/Discount.js';
 import DiscountManager from './Domain/Discount/DiscountManager.js';
 import DiscountView from './Domain/Discount/DiscountView.js';
 import EventController from './Controller/EventController.js';
-import OrderManager from './Service/OrderManager.js';
 import View from './View/View.js';
 
 class App {
@@ -12,12 +11,11 @@ class App {
 
   constructor() {
     const view = new View();
-    const orderManager = new OrderManager(view);
     const discount = new Discount(discounts, promotion);
     const discountManager = new DiscountManager(discount);
     const discountView = new DiscountView(view);
 
-    this.#eventController = new EventController(view, orderManager, discountManager, discountView);
+    this.#eventController = new EventController(view, discountManager, discountView);
   }
 
   async run() {
