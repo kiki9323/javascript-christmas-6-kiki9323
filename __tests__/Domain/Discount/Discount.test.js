03dea296c08b22a584e45acef36694c844ae4231
noodle <noodle>
2023-11-15 02:52:46 +0900

    test: Discount.test.js 테스트 코드 수정 및 케이스 추가

--- __tests__/Domain/Discount/Discount.test.js
+++ __tests__/Domain/Discount/Discount.test.js
@@ -30,30 +30,48 @@ describe('Discount 클래스', () => {
 
   const discount = new Discount(discounts, promotion);
 
-  describe('getDdayDiscount 메소드', () => {
-    // when
-    const christmasDiscount = discount.getDdayDiscount(25);
-
-    // then
+  describe('getDdayDiscount 메서드', () => {
     test('25일에 주문하면 크리스마스 할인이 적용되어야 한다.', () => {
+      // when - then
+      const christmasDiscount = discount.getDdayDiscount(25);
       expect(christmasDiscount).toBe(1000 + (25 - 1) * 100);
+    });
+
+    test('26일 부터의 주문은 크리스마스 할인이 적용되지 않아야 한다.', () => {
+      // when - then
+      const christmasDiscount = discount.getDdayDiscount(26);
+      expect(christmasDiscount).toBe(0);
     });
   });
 
-  describe('getWeeklyDiscount 메소드', () => {
-    // when
-    const menuCategory = { 디저트: 5 };
-    const weekdayDiscount = discount.getWeeklyDiscount(3, menuCategory, 'weekday');
-
-    // then
+  describe('getWeeklyDiscount 메서드', () => {
     test('평일에 디저트를 5개 주문하면 평일 할인이 적용되어야 한다.', () => {
+      // when
+      const orderDate = 4;
+      const menuCategory = { 디저트: 5 };
+      const weekdayDiscount = discount.getWeeklyDiscount(orderDate, menuCategory, 'weekday');
+
+      // then
       expect(weekdayDiscount).toBe(2023 * 5);
     });
   });
 
-  describe('getSpecialDiscount 메소드', () => {
+  describe('getWeeklyDiscount 메서드', () => {
+    test('주말에 메인들 3개 주문하면 주말 할인이 적용되어야 한다.', () => {
+      // when
+      const orderDate = 8;
+      const menuCategory = { 메인: 3 };
+      const weekendDiscount = discount.getWeeklyDiscount(orderDate, menuCategory, 'weekend');
+
+      // then
+      expect(weekendDiscount).toBe(2023 * 3);
+    });
+  });
+
+  describe('getSpecialDiscount 메서드', () => {
     // when
-    const specialDiscount = discount.getSpecialDiscount(24);
+    const orderDate = 24;
+    const specialDiscount = discount.getSpecialDiscount(orderDate);
 
     // then
     test('특별 할인 날에 주문하면 특별 할인이 적용되어야 한다.', () => {
@@ -61,7 +79,20 @@ describe('Discount 클래스', () => {
     });
   });
 
-  describe('applyPromotion 메소드', () => {
+  describe('getSpecialDiscount, getWeeklyDiscount 메서드', () => {
+    // when
+    const orderDate = 10;
+    const menuCategory = { 디저트: 4 };
+    const specialDiscount = discount.getSpecialDiscount(orderDate);
+    const weekdayDiscount = discount.getWeeklyDiscount(orderDate, menuCategory, 'weekday');
+
+    // then
+    test('달력에 별이 있는 특별 할인 + 평일 할인(디저트) 할인 적용되어야 한다.', () => {
+      expect(specialDiscount + weekdayDiscount).toBe(1000 + 2023 * 4);
+    });
+  });
+
+  describe('applyPromotion 메서드', () => {
     // when
     const promotionDiscount = discount.applyPromotion(130000);
 
