74041545b7a2479414555431cacd1ed337813396
noodle <noodle>
2023-11-14 12:05:25 +0900

    test: DiscountManager.test.js 테스트 코드 작성

--- __tests__/Domain/Discount/DiscountManager.test.js
+++ __tests__/Domain/Discount/DiscountManager.test.js
@@ -58,3 +58,44 @@ describe('DiscountManager 클래스', () => {
     });
   });
 
+  describe('applyDiscount 메서드', () => {
+    // given
+    const orderDate = 3;
+    const menuCategory = { 디저트: 5 };
+
+    // when
+    discountManager.setTotalAmount(130000);
+    const appliedDiscount = discountManager.applyDiscount(orderDate, menuCategory);
+
+    // then
+    test('주문 날짜와 메뉴 카테고리, 특별 할인 따라 할인이 적용되어야 한다.', () => {
+      expect(appliedDiscount).toMatchObject({
+        '크리스마스 디데이 할인': 1000 + (3 - 1) * 100,
+        '평일 할인': 2023 * 5,
+        '주말 할인': 0,
+        '특별 할인': 1000,
+        '증정 이벤트': 25000,
+      });
+    });
+  });
+
+  describe('getTotalDiscount 메서드', () => {
+    // when
+    const totalDiscount = discountManager.getTotalDiscount();
+
+    // then
+    test('적용된 모든 할인 금액을 합쳐야 한다.', () => {
+      expect(totalDiscount).toBe(1000 + (3 - 1) * 100 + 2023 * 5 + 25000 + 1000);
+    });
+  });
+
+  describe('getTotalDiscountWithoutGift 메서드', () => {
+    // when
+    const totalDiscountWithoutGift = discountManager.getTotalDiscountWithoutGift();
+
+    // then
+    it('증정 이벤트를 제외한 모든 할인 금액을 합쳐야 한다.', () => {
+      expect(totalDiscountWithoutGift).toBe(1000 + (3 - 1) * 100 + 2023 * 5 + 1000);
+    });
+  });
+});
