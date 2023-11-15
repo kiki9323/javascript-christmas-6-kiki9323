8c4a21a64471495d2e9c0e31c5417d94c57a201a
noodle <noodle>
2023-11-15 17:47:43 +0900

    style: 불필요한 코드와 공백 정리

--- src/Domain/Discount/DiscountView.js
+++ src/Domain/Discount/DiscountView.js
@@ -17,19 +17,16 @@ class DiscountView {
     this.finalPayment(totalPriceBeforeDiscount, totalDiscountWithoutGift);
   }
 
-  // <혜택 내역> : 할인들과 증정 이벤트 보여줌
   discountBenefits(appliedDiscount, totalPriceBeforeDiscount) {
     this.#view.printDiscountBenefits();
     this.#view.print(DiscountMsgFormat.printDiscountMessage(appliedDiscount, totalPriceBeforeDiscount));
   }
 
-  // <총혜택 금액> : 할인들과 증정 이벤트가격 모두 더해서 보여줌
   totalDiscountAmount(totalDiscount) {
     this.#view.printTotalDiscountAmount();
     this.#view.print(totalDiscount ? `-${MessageFormat.wonPrice(totalDiscount)}` : '0원');
   }
 
-  // <할인 후 예상 결제 금액> = 총혜택 금액 - 증정 이벤트
   finalPayment(totalPriceBeforeDiscount, totalDiscountWithoutGift) {
     const finalPayment = totalPriceBeforeDiscount - totalDiscountWithoutGift;
     this.#view.printFinalPaymentAmountAfterDiscount();
